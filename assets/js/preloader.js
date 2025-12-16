afa19aba548eda11ac1ea567c5baf9452ad00a28
Mykola <hedonist.boutique.kiev@gmail.com>
2025-12-16 11:30:44 +0300

    Add global preloader markup and loader

---
+++ assets/js/preloader.js
@@ -0,0 +1,15 @@
+(function () {
+  function hide() {
+    const el = document.getElementById('preloader');
+    if (!el) return;
+    el.classList.add('is-hidden');
+    // можно удалить из DOM после анимации
+    setTimeout(() => el.remove(), 600);
+  }
+
+  // Скрывать строго после полной загрузки (картинки/видео/шрифты)
+  window.addEventListener('load', hide, { once: true });
+
+  // Страховка: если load почему-то не приходит
+  setTimeout(hide, 8000);
+})();
